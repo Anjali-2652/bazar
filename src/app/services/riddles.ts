dcaa96f3caf53cf4b33e1c7c1ef0662b5a1614f1
Anjali Gupta <guptaanjali2651@gmail.com>
2026-09-18 14:10:32 +0545

    updated

--- src/app/services/riddles.ts
+++ src/app/services/riddles.ts
@@ -1,27 +1,27 @@
-// import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class Riddles {
-//   private apiUrl = 'https://api.api-ninjas.com/v1/riddles';
-//   private apiKey = ' qrHfBREroVl2cDG1vIddMMia4yMnS5MRy0atfBYO'
+@Injectable({
+  providedIn: 'root',
+})
+export class Riddles {
+  private apiUrl = 'https://api.api-ninjas.com/v1/riddles';
+  private apiKey = ' qrHfBREroVl2cDG1vIddMMia4yMnS5MRy0atfBYO'
 
-//   constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){}
 
-//   private getHeaders(): HttpHeaders{
-//     return new HttpHeaders({
-//       'Content-Type' : 'application/json',
-//       'X-Api-Key' : this.apiKey
-//     });
-//   }
-//   getRiddles():Observable<any>{
-//     return this.http.get<any>(this.apiUrl, {headers:this.getHeaders()}).subscribe({
-//       next:(resData)=>{
-//         console.log(resData)
-//       }
-//     });
-//   }
-// }
+  private getHeaders(): HttpHeaders{
+    return new HttpHeaders({
+      'Content-Type' : 'application/json',
+      'X-Api-Key' : this.apiKey
+    });
+  }
+  getRiddles():Observable<any>{
+    return this.http.get<any>(this.apiUrl, {headers:this.getHeaders()}).subscribe({
+      next:(resData)=>{
+        console.log(resData)
+      }
+    });
+  }
+}
